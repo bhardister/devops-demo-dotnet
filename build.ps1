5ca82fbf2cad1d969ba435c2039b1a2d2167b196
bhardister <bobhardister.austin@gmail.com>
2017-08-03 06:22:28 +0000

    fix syntax

--- build.ps1
+++ build.ps1
@@ -2,10 +2,9 @@ param(
 	[string]$rev = "1"
 )
 
-cd C:\_s\devops-demo-dotnet
 .paket/paket restore
 Push-Location devops-demo-dotnet
 .\package.cmd $rev
-rm ..\octo\*.*
+If (Test-Path ..\octo) { rm ..\octo\*.* }
 nuget pack -Version "0.0.$rev" -OutputDirectory ..\octo
 Pop-Location
