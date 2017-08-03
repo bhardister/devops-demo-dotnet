4b0a090d11c8b14aa9fad53a82d31e94004f67c6
bhardister <bobhardister.austin@gmail.com>
2017-08-03 00:25:43 +0000

    add build.ps1

---
+++ build.ps1
@@ -0,0 +1,6 @@
+﻿cd C:\_s\devops-demo-dotnet
+.paket/paket restore
+Push-Location devops-demo-dotnet
+.\package.cmd
+nuget pack -Version 0.0.1 -OutputDirectory ..\octo
+Pop-Location
--- devops-demo-dotnet/package.cmd
+++ devops-demo-dotnet/package.cmd
@@ -1,22 +1,3 @@
 @echo off
 DEL /F /Q /S AwsBundle\*.*
-set year=%date:~-2%
-call :trim %year% year
-set month=%date:~4,2%
-call :trim %month% month
-set day=%date:~7,2%
-call :trim %day% day
-set hour=%time:~0,2%
-call :trim %hour% hour
-set min=%time:~3,2%
-call :trim %min% min
-set datetimef=%year%%month%%day%%hour%%min%
-REM echo %datetimef%
-"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" devops-demo-dotnet.csproj /T:Package /P:Configuration="Debug";Platform="AnyCPU";IncludeIisSettings=false;PackageLocation=AwsBundle\demo-webapp.0.0.1-dev%datetimef%.zip
-REM pause
-goto eof
-
-:trim
-set %2=%1
-
-:eof
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" devops-demo-dotnet.csproj /T:Package /P:Configuration="Debug";Platform="AnyCPU";IncludeIisSettings=false;PackageLocation=..\AwsBundle\demo-webapp.0.0.1-dev%datetimef%.zip
