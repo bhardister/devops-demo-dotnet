fbafce37bdc01dd181a98ca2c950ab170f33b04c
bhardister <bobhardister.austin@gmail.com>
2017-08-03 22:14:12 +0000

    use dev and test

--- deploy-to-iis.ps1
+++ deploy-to-iis.ps1
@@ -7,5 +7,5 @@ $p = "octo\devops-demo-dotnet.0.0." + $rev + ".nupkg"
 $v = "0.0." + $rev
 octo push --package $p --replace-existing --server http://172.31.23.165:8080 -apiKey $apiKey
 Write-Host ("deploy-to-iis: push of package at $p to Octopus library complete")
-octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto intg --server http://172.31.23.165:8080 -apiKey $apiKey
-Write-Host ("deploy-to-iis END: deploying devops-demo-dotnet." + $v + " to IIS intg environment")
+octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto dev --server http://172.31.23.165:8080 -apiKey $apiKey
+Write-Host ("deploy-to-iis END: deploying devops-demo-dotnet." + $v + " to IIS dev environment")
