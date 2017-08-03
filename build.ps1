9120ad8fbc776999d04b7a3060849741d846e27b
bhardister <bobhardister.austin@gmail.com>
2017-08-03 04:01:12 +0000

    fix

--- build.ps1
+++ build.ps1
@@ -1,6 +1,11 @@
-﻿cd C:\_s\devops-demo-dotnet
+param(
+	[string]$rev = "1"
+)
+
+cd C:\_s\devops-demo-dotnet
 .paket/paket restore
 Push-Location devops-demo-dotnet
-.\package.cmd
-nuget pack -Version 0.0.1 -OutputDirectory ..\octo
+.\package.cmd $rev
+rm ..\octo\*.*
+nuget pack -Version "0.0.$rev" -OutputDirectory ..\octo
 Pop-Location
--- devops-demo-dotnet/package.cmd
+++ devops-demo-dotnet/package.cmd
@@ -1,3 +1,4 @@
 @echo off
-DEL /F /Q /S AwsBundle\*.*
-"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" devops-demo-dotnet.csproj /T:Package /P:Configuration="Debug";Platform="AnyCPU";IncludeIisSettings=false;PackageLocation=..\AwsBundle\demo-webapp.0.0.1-dev%datetimef%.zip
+set rev=%1
+DEL /F /Q /S ..\AwsBundle\*.*
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" devops-demo-dotnet.csproj /T:Package /P:Configuration="Debug";Platform="AnyCPU";IncludeIisSettings=false;PackageLocation=..\AwsBundle\demo-webapp.0.0.%rev%.zip
