c656736c9f3731425cd3a30fc78fb94c2580bdf5
Bob Hardister <bob.hardister@shipstation.com>
2017-12-05 13:00:50 -0600

    Update for Octotest

--- deploy-to-iis.ps1
+++ deploy-to-iis.ps1
@@ -1,11 +1,11 @@
 param(
 	[string]$rev = "0",
-	[string]$apiKey = $env:apiKey
+	[string]$octoTestApiKey
 )
 Write-Host ("deploy-to-iis START: rev = $rev")
 $p = "octo\devops-demo-dotnet.0.0." + $rev + ".nupkg"
 $v = "0.0." + $rev
 octo push --package $p --replace-existing --server http://172.31.23.165:8080 -apiKey $apiKey
 Write-Host ("deploy-to-iis: push of package at $p to Octopus library complete")
-octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto dev --server http://172.31.23.165:8080 -apiKey $apiKey
+octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto dev --server https://octotest.sslocal.com -apiKey $octoTestApiKey
 Write-Host ("deploy-to-iis END: deploying devops-demo-dotnet." + $v + " to IIS dev environment")
