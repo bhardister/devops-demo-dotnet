b511da6fa50f316891538c73cc82a3431e42dbc5
Bob Hardister <bob.hardister@shipstation.com>
2017-12-05 14:32:46 -0600

    add tenant argument to create-release

--- deploy-to-iis.ps1
+++ deploy-to-iis.ps1
@@ -5,7 +5,7 @@ param(
 Write-Host ("deploy-to-iis START: rev = $rev")
 $p = "octo\devops-demo-dotnet.0.0." + $rev + ".nupkg"
 $v = "0.0." + $rev
-octo push --package $p --replace-existing --server http://172.31.23.165:8080 -apiKey $apiKey
+octo push --package $p --replace-existing --server https://octotest.sslocal.com -apiKey $octoTestApiKey
 Write-Host ("deploy-to-iis: push of package at $p to Octopus library complete")
-octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto dev --server https://octotest.sslocal.com -apiKey $octoTestApiKey
+octo create-release --project devops-demo-dotnet --version $v --package "devops-demo-dotnet:$v" --deployto dev --tenant Z --server https://octotest.sslocal.com -apiKey $octoTestApiKey
 Write-Host ("deploy-to-iis END: deploying devops-demo-dotnet." + $v + " to IIS dev environment")
